8e1f5b2d063ee763c67608ad55f36aacec9fd49d
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2021-04-28 21:46:45 +0300

    optimizing omp

--- linalg.c
+++ linalg.c
@@ -16,18 +16,32 @@ void dgemm(const double *A, const double *B, double *C, const int M, const int N
 #else
     int i, j, k;
     double sum;
-    /*
-     * FILLME: Parallelize the code.
-     */
-    #pragma omp parallel for private(i,j,k,sum) collapse(2)
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
-            sum = 0.;
-            for (k = 0; k < K; k++)
-                sum += A[i * K + k] * B[k * N + j];
-            C[i * N + j] = sum;
+    // #pragma omp parallel for private(i,j,k,sum) collapse(2)
+    // for (i = 0; i < M; i++) {
+    //     for (j = 0; j < N; j++) {
+    //         sum = 0.;
+    //         for (k = 0; k < K; k++)
+    //             sum += A[i * K + k] * B[k * N + j];
+    //         C[i * N + j] = sum;
+    //     }
+    // }
+    const int TILE_SIZE = 32;
+    #pragma omp parallel for collapse(2) schedule(static)
+    for (ii = 0; ii < M; ii += TILE_SIZE) {
+        for (jj = 0; jj < N; jj += TILE_SIZE) {
+            for (i = ii; i < max(ii + TILE_SIZE, M); ++i) {
+                for (j = jj; j < max(jj + TILE_SIZE, N); ++j) {
+                    cij = C[j * M + i];
+                    sum = 0.;
+                    for (k = 0; k < K; ++k) {
+                        sum += A[i * K + k] * B[k * N + j];
+                    }
+                    C[i * N + j] = sum;
+                }
+            }
         }
     }
+
 #endif
 }
 
@@ -70,10 +84,10 @@ void dgemm_tb(const double *A, const double *B, const double *C, double *D, cons
      *  FILLME: Use cblas_dgemm()
      */
     double alpha, beta;
-    
+
     // first I do: D = C
     memcpy(D, C, M * N * sizeof(double));
-    
+
     // then I do: D = 1 * A*B' + 1 * D
     alpha = 1;
     beta = 1;
