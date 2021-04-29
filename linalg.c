9298f1dc2164bfb13ce962cd0f8e0612e65afe0a
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2021-04-29 10:45:14 +0300

    optimizing omp

--- linalg.c
+++ linalg.c
@@ -67,20 +67,40 @@ void dgemm_ta(const double *A, const double *B, double *C, const int M, const in
                 M, N, K, alpha, A, M, B, N, beta, C, N);
     // A: K elements per row, M rows
 #else
-    int i, j, k;
+    int i, j, k, ii, jj, kk;
     double sum;
     /*
      * FILLME: Parallelize the code.
      */
-    #pragma omp parallel for private(i,j,k,sum) collapse(2)
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
-            sum = 0.;
-            for (k = 0; k < K; k++)
-                sum += A[k * M + i] * B[k * N + j];
-            C[i * N + j] = sum;
+    // #pragma omp parallel for private(i,j,k,sum) collapse(2)
+    // for (i = 0; i < M; i++) {
+    //     for (j = 0; j < N; j++) {
+    //         sum = 0.;
+    //         for (k = 0; k < K; k++)
+    //             sum += A[k * M + i] * B[k * N + j];
+    //         C[i * N + j] = sum;
+    //     }
+    // }
+    
+    #pragma omp parallel for collapse(2) private(i,j,k,ii,jj,kk,sum)
+    for (ii = 0; ii < M; ii += TILE_SIZE) {
+        for (jj = 0; jj < N; jj += TILE_SIZE) {
+            for (kk = 0; kk < K; kk += TILE_SIZE) {
+                for (i = ii; i < MIN(ii + TILE_SIZE, M); ++i) {
+                    for (j = jj; j < MIN(jj + TILE_SIZE, N); ++j) {
+                        // cij = C[j * M + i];
+                        sum = 0.;
+                        #pragma omp unroll 16
+                        for (k = kk; k < MIN(kk + TILE_SIZE, K); ++k) {
+                            sum += A[k * M + i] * B[k * N + j];
+                        }
+                        C[i * N + j] += sum;
+                    }
+                }
+            }
         }
     }
+
 #endif
 }
 
@@ -103,18 +123,36 @@ void dgemm_tb(const double *A, const double *B, const double *C, double *D, cons
                 M, N, K, alpha, A, K, B, K, beta, D, N);
     // A: K elements per row, M rows
 #else
-    int i, j, k;
+    int i, j, k, ii, jj, kk;
     double sum;
     /*
      * FILLME: Parallelize the code.
      */
-    #pragma omp parallel for private(i,j,k,sum) collapse(2)
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
-            sum = 0.;
-            for (k = 0; k < K; k++)
-                sum += A[i * K + k] * B[j * K + k];
-            D[i * N + j] = sum + C[i * N + j];
+    // #pragma omp parallel for private(i,j,k,sum) collapse(2)
+    // for (i = 0; i < M; i++) {
+    //     for (j = 0; j < N; j++) {
+    //         sum = 0.;
+    //         for (k = 0; k < K; k++)
+    //             sum += A[i * K + k] * B[j * K + k];
+    //         D[i * N + j] = sum + C[i * N + j];
+    //     }
+    // }
+    #pragma omp parallel for collapse(2) private(i,j,k,ii,jj,kk,sum)
+    for (ii = 0; ii < M; ii += TILE_SIZE) {
+        for (jj = 0; jj < N; jj += TILE_SIZE) {
+            for (kk = 0; kk < K; kk += TILE_SIZE) {
+                for (i = ii; i < MIN(ii + TILE_SIZE, M); ++i) {
+                    for (j = jj; j < MIN(jj + TILE_SIZE, N); ++j) {
+                        // cij = C[j * M + i];
+                        sum = 0.;
+                        #pragma omp unroll 16
+                        for (k = kk; k < MIN(kk + TILE_SIZE, K); ++k) {
+                            sum += A[i * K + k] * B[j * K + k];
+                        }
+                        D[i * N + j] += sum + C[i * N + j];
+                    }
+                }
+            }
         }
     }
 #endif
